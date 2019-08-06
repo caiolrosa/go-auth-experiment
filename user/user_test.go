534de659c5647e52e2829eac71665b7c92fa771f
Caio Rosa <caiolrosa1912@gmail.com>
2019-08-06 13:45:48 -0300

    Update tests changing the test case test to input

--- user/user_test.go
+++ user/user_test.go
@@ -6,54 +6,54 @@ import (
 
 func TestEncryptPassword(t *testing.T) {
 	testCases := []struct {
-		test     User
+		input    User
 		expected error
 	}{
 		{
-			test:     User{Password: "12345678"},
+			input:    User{Password: "12345678"},
 			expected: nil,
 		},
 		{
-			test:     User{Password: "qwertyasdfzxcv"},
+			input:    User{Password: "qwertyasdfzxcv"},
 			expected: nil,
 		},
 	}
 
 	for _, testCase := range testCases {
-		got := testCase.test.EncryptPassword()
+		got := testCase.input.EncryptPassword()
 		if got != testCase.expected {
-			t.Errorf("\nFor input: %v\nExpected: %v\nGot: %v", testCase.test, testCase.expected, got)
+			t.Errorf("\nFor input: %v\nExpected: %v\nGot: %v", testCase.input, testCase.expected, got)
 		}
 	}
 }
 
 func TestValid(t *testing.T) {
 	testCases := []struct {
-		test     User
+		input    User
 		expected bool
 	}{
 		{
-			test:     User{Name: "test", Email: "test@test.com", Password: "12345678"},
+			input:    User{Name: "test", Email: "test@test.com", Password: "12345678"},
 			expected: true,
 		},
 		{
-			test:     User{Name: "", Email: "test@test.com", Password: "12345678"},
+			input:    User{Name: "", Email: "test@test.com", Password: "12345678"},
 			expected: false,
 		},
 		{
-			test:     User{Name: "test", Email: "", Password: "12345678"},
+			input:    User{Name: "test", Email: "", Password: "12345678"},
 			expected: false,
 		},
 		{
-			test:     User{Name: "test", Email: "test@test.com", Password: ""},
+			input:    User{Name: "test", Email: "test@test.com", Password: ""},
 			expected: false,
 		},
 	}
 
 	for _, testCase := range testCases {
-		got := testCase.test.Valid()
+		got := testCase.input.Valid()
 		if got != testCase.expected {
-			t.Errorf("\nFor input: %v\nExpected: %t\nGot: %t", testCase.test, testCase.expected, got)
+			t.Errorf("\nFor input: %v\nExpected: %t\nGot: %t", testCase.input, testCase.expected, got)
 		}
 	}
 }
--- utils/validators_test.go
+++ utils/validators_test.go
@@ -4,39 +4,39 @@ import "testing"
 
 func TestValidateEmail(t *testing.T) {
 	testCases := []struct {
-		test     string
+		input    string
 		expected bool
 	}{
-		{test: "test@test.com", expected: true},
-		{test: "t@t.c", expected: true},
-		{test: "test", expected: false},
-		{test: "@", expected: false},
-		{test: "", expected: false},
+		{input: "test@test.com", expected: true},
+		{input: "t@t.c", expected: true},
+		{input: "test", expected: false},
+		{input: "@", expected: false},
+		{input: "", expected: false},
 	}
 
 	for _, testCase := range testCases {
-		got := ValidateEmail(testCase.test)
+		got := ValidateEmail(testCase.input)
 		if got != testCase.expected {
-			t.Errorf("\nFor input: %s\nExpected: %t\nGot: %t", testCase.test, testCase.expected, got)
+			t.Errorf("\nFor input: %s\nExpected: %t\nGot: %t", testCase.input, testCase.expected, got)
 		}
 	}
 }
 func TestValidatePassword(t *testing.T) {
 	testCases := []struct {
-		test     string
+		input    string
 		expected bool
 	}{
-		{test: "12345678", expected: true},
-		{test: "qwertyasdfgzxcvb", expected: true},
-		{test: "1234567", expected: false},
-		{test: "wasd", expected: false},
-		{test: "", expected: false},
+		{input: "12345678", expected: true},
+		{input: "qwertyasdfgzxcvb", expected: true},
+		{input: "1234567", expected: false},
+		{input: "wasd", expected: false},
+		{input: "", expected: false},
 	}
 
 	for _, testCase := range testCases {
-		got := ValidatePassword(testCase.test)
+		got := ValidatePassword(testCase.input)
 		if got != testCase.expected {
-			t.Errorf("\nFor input: %s\nExpected: %t\nGot: %t", testCase.test, testCase.expected, got)
+			t.Errorf("\nFor input: %s\nExpected: %t\nGot: %t", testCase.input, testCase.expected, got)
 		}
 	}
 }
