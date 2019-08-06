52e059626e2ad6371639123b9c567a7ad1b341d4
Caio Rosa <caiolrosa1912@gmail.com>
2019-08-06 03:57:08 -0300

    Add logs to migrations

--- db/migrations/20190805_InitialMigration.go
+++ db/migrations/20190805_InitialMigration.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"guardian-api/db"
 	"guardian-api/user"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // InitialMigration creates the base User schema
 func initialMigration() {
+	log.Info("Applying 20190805_InitialMigration")
 	db, err := db.GetConnection()
 	if err != nil {
 		fmt.Println(err)
--- db/migrations/run_migrations.go
+++ db/migrations/run_migrations.go
@@ -1,6 +1,10 @@
 package migrations
 
+import log "github.com/sirupsen/logrus"
+
 // Migrate runs all migrations
 func Migrate() {
+	log.Info("Starting to apply migrations...")
 	initialMigration()
+	log.Info("Finished applying migrations.")
 }
